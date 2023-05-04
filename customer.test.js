4d1f4bca1464d6ae9b7571d0b9a07ba494e1ffa5
klb545 <126502552+klb545@users.noreply.github.com>
2023-05-04 22:23:29 +0100

    added test - 'customer is unable to buy car because they currently possess a car'

--- customer.test.js
+++ customer.test.js
@@ -20,7 +20,7 @@ describe('getters', () => {
 });
 
 
-describe('customer can buy car if they have enough money', () => {
+describe('customer can buy car if they have enough money and if they don\'t currently possess a car (...in a world where you can only own one car)', () => {
 
     test('customer can buy a car because they have more in their wallet than the value of the car', () => {
         customer.buyCar(peugot);
@@ -45,6 +45,16 @@ describe('customer can buy car if they have enough money', () => {
         customer.buyCar(toyota);
         expect(customer.wallet).toBe(10000);
         expect(customer.car).toEqual(null);
+    });
+
+    test('customer is unable to buy car because they currently possess a car', () => {
+        customer.car = toyota;
+        customer.wallet = 25000;
+        dealer.stock = [toyota, peugot];
+        //
+        customer.buyCar(toyota);
+        expect(customer.wallet).toBe(25000);
+        expect(customer.car).toEqual(toyota);
     });
     
 });
